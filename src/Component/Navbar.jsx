02a1e74bac0e637d047900f47094333198b19850
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-02-22 12:42:56 +0530

    Navbar:- Added logo and increased text-size

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,30 +1,29 @@
 import React, {useState} from 'react'
 
 function Navbar() {
-  const [isDarkMode, setDarkMode] = React.useState(false)
+  const [isDarkMode, setDarkMode] = React.useState(true)
   return (
     <div>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-base-100 px-20">
         <div className="navbar-start">
           <div className="dropdown">
             
             <label tabIndex={0} className="btn btn-ghost btn-circle">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
             </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li><a>Film</a></li>
-              <li><a>Tv</a></li>
-              <li><a>Podcast</a></li>
-              <li><a>Merchandise</a></li>
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-60 ">
+              <li><a className='text-base'>Film</a></li>
+              <li><a className='text-base'>Tv</a></li>
+              <li><a className='text-base'>Podcast</a></li>
+              <li><a className='text-base'>Merchandise</a></li>
             </ul>
           </div>
         </div>
         <div className="navbar-center">
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+          <img src="assets/Images/Navbar/Logo.jpeg" alt="" className='h-12 rounded-md' />
         </div>
         <div className="navbar-end">
-          {/*<img src={isDarkMode ? "assets/Images/Navbar/Moon.svg " : " assets/Images/Navbar/Sun.svg "} alt="image" className="w-4 h-4 " />*/}
-          <p className='mr-4'>Light Mode</p>
+          <img src={isDarkMode ? "assets/Images/Navbar/Moon.svg " : " assets/Images/Navbar/Sun.svg "} alt="image" className="w-6 h-6 mr-4" />
           <label className="relative inline-flex items-center ">
             <input
               type="checkbox"
