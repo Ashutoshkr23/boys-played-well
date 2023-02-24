088d12d273f6154093c28a2aa65e617049d18671
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-02-24 15:15:58 +0530

    fade in fade out

--- src/Component/Merchandise.jsx
+++ src/Component/Merchandise.jsx
@@ -4,20 +4,16 @@ import { motion } from 'framer-motion';
 function Merchandise() {
     const fadeSlideIn = {
         initial: {
-            x: 50,
-            opacity: 0
+         opacity: 0
         },
         animate: {
-            x: 0,
             opacity: 1,
             transition: {
-                duration: 1,
-                type: 'spring',
-                bounce: 0.5
+                duration: 5,
+                
             }
         },
         exit: {
-            x: -50,
             opacity: 0
         }
     }
