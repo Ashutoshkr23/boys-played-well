fb46c65886b1983b4ceb8cd64a2ad63f30ccb8d6
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-03-05 01:35:36 +0530

    Main Section Complete

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -4,26 +4,30 @@ import { motion } from 'framer-motion';
 function Main() {
   return (
 
-    <div className='relative  bg-black'>
-      <div className='py-20 md:py-28 lg:py-40 px-10 md:px-28 lg:px-40 text-center bg-black text-white'>
-        <h1 className='text-xl md:text-4xl  lg:text-5xl font-bold'>FILMS , TV , PODCAST</h1>
-        <h2 className='text-lg md:text-3xl lg:text-4xl mt-4 md:mt-8 font-bold'>Boys Played Well.</h2>
-      </div>
-      <div className='-mt-60 fixed'>
-        <div class="flex flex-col bg-transparent text-slate-200 w-24">
-          <div class="nav-link-container mt-2 pb-1 border-b-2 border-white ">
-            <a href="#movie" class="nav-link text-lg pl-4 hover:text-white">Films</a>
+    <div className='relative  bg-black text-white mt-10 pb-10'>
+      <div className='flex flex-col justify-center items-center'>
+        <h1 className='font-extrabold text-[160px]  leading-none' >BPW</h1>
+        <h6 className='mx-auto mt-2 font-light text-xl tracking-[.7em]'>Boys Played Well</h6>
+        <div className='flex w-full mt-20 items-center'>
+          <div className='flex-1 h-[1px] bg-white'></div>
+          <div>
+            <h1 className='tracking-[.25em] text-4xl mx-8'>FILMS | TV | PODCAST</h1>
           </div>
-          <div class="nav-link-container mt-2 pb-1 border-b-2 border-white">
-            <a href="#tv" class="nav-link text-lg pl-4 hover:text-white">TV</a>
-          </div>
-          <div class="nav-link-container mt-2 pb-1 border-b-2 border-white ">
-            <a href="#podcast" class="nav-link text-lg pl-4 hover:text-white">Podcast</a>
-          </div>
-          <div class="nav-link-container mt-2 pb-1 border-b-2 border-white ">
-            <a href="#about" class="nav-link text-lg pl-4 hover:text-white">About</a>
-          </div>
+          <div className='flex-1 h-[1px] bg-white'></div>
         </div>
+        <button className='tracking-[.25em] rounded-md mt-20 w-44 h-11 ' style={{
+          border: '1px solid transparent',
+          borderRadius: '8px',
+          borderImage: 'linear-gradient(to right, #ED5266, #FF843F) 1',  
+        }}>
+          Support
+        </button>
+        <ul className="flex flex-col items-center">
+          <li className='h-3 w-3 border border-white rounded-md mt-8'></li>
+          <li className='h-2.5 w-2.5 border border-white border-opacity-60 rounded-[5px] mt-8 '></li>
+          <li className='h-2 w-2 border border-white border-opacity-30 rounded-[4px] mt-8'></li>
+        </ul>
+        
       </div>
     </div>
   )
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -4,16 +4,16 @@ import React, {useState} from 'react'
 function Navbar() {
   const [isDarkMode, setDarkMode] = React.useState(true)
   return (
-    <div className='bg-black text-white px-28 py-5'>
+    <div className='text-white px-28 py-5'>
       <div className='flex justify-between'>
         <div><h1 className='text-white text-2xl font-extrabold'>BPW</h1></div>
         <div>
           <ul className='flex gap-8 '>
-            <li><a className='text-base'>FILM</a></li>
-            <li><a className='text-base'>TV</a></li>
-            <li><a className='text-base'>PODCAST</a></li>
-            <li><a className='text-base'>ABOUT</a></li>
-            <li><a className='text-base'>TEAM</a></li>
+            <li><a className='text-sm tracking-[.25em]'>FILM</a></li>
+            <li><a className='text-sm tracking-[.25em]'>TV</a></li>
+            <li><a className='text-sm tracking-[.25em]'>PODCAST</a></li>
+            <li><a className='text-sm tracking-[.25em]'>ABOUT</a></li>
+            <li><a className='text-sm tracking-[.25em]'>TEAM</a></li>
           </ul>
         </div>
       </div>
