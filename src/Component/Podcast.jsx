e1e61b4aac0772a997f05894a2dc4009d86139a0
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-03-05 18:01:22 +0530

    Podcast Component Partial Built

--- src/Component/Podcast.jsx
+++ src/Component/Podcast.jsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
 import React from 'react'
+import PodcastComponent from '../Component/PodcastComponent'
+
 
 function Podcast() {
   return (
     <div id='podcast'>
       <div className='flex flex-col my-28 items-center text-white'>
         <h1 className='text-[40px] '>Podcast</h1>
+        <PodcastComponent/>
       </div>
     </div>
   )
