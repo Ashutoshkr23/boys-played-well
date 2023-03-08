6bd493748d80b4977417b4b5b4bebf8d3428e6ea
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-03-08 13:23:56 +0530

    hover effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
         'orange-1': '0 0px 30px rgba(236, 129, 64, 0.5)',
         'green-1': '0 0px 30px rgba(0, 153, 102, 0.5)',
         'green-2': '0 0px 30px rgba(76, 137, 104, 0.5)',
+        'text': '0 0px 6px rgba(255, 147, 87, 1)',
+
       }
       },
     },
