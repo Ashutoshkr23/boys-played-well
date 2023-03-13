424a7bfbc42e738f9bb073fa0ab7b1564a0c124c
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-03-13 21:02:11 +0530

    modified:   src/Component/Landing.jsx

--- src/Component/Landing.jsx
+++ src/Component/Landing.jsx
@@ -14,7 +14,7 @@ function Landing() {
     }, [])
   return (
       <div style={{ display: showVideo ? 'block' : 'none' }}>
-      <div className='h-screen w-full lg:w-3/5 mx-auto text-white pb-10'>
+      <div className='h-screen w-full lg:w-3/5 mx-auto text-white'>
               <img  src="/assets/Images/Main/Logo.gif" className=' object-cover'></img>
       </div>
     </div>
