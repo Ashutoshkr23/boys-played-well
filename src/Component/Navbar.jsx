04cda83e245d85820a42ff7c15e50a78b90eca30
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-03-13 10:29:14 +0530

    Animation For nav

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React, {useState , useEffect} from 'react'
 import { motion, AnimatePresence } from 'framer-motion';
+import { exit } from 'process';
 
 
 function Navbar() {
@@ -16,6 +17,21 @@ function Navbar() {
     getYear();
   }, [])
 
+  const fadeIn = {
+    initial: {
+      opacity: 0
+    },
+    animate: {
+      opacity: 1,
+      transition: {
+        duration: 0.3
+      }
+    },
+    exit : {
+      opacity: 0,
+    }
+  }
+
   return (
     <div className='text-white'>
       <div className='lg:hidden '>
@@ -28,19 +44,20 @@ function Navbar() {
                 <div className={`relative border-none mt-6 mr-6 z-50  bg-transparent`}>
                   <div className='  h-3.5 w-5'>
                     <div
-                      className={` h-0.5 bg-white w-5 origin-top-left rounded-md ${isOpen ? 'rotate-45 translate-x-px' : 'rotate-0'}`}
+                      className={` h-0.5 bg-white w-5 origin-top-left ease-in duration-200 rounded-md ${isOpen ? 'rotate-45 translate-x-px' : 'rotate-0'}`}
                     />
                     <div
-                      className={` h-0.5 w-5 bg-white rounded-md mt-1 ${isOpen ? 'hidden' : 'block'}`}
+                      className={` h-0.5 w-5 bg-white ease-in duration-200 rounded-md mt-1 ${isOpen ? 'hidden' : 'block'}`}
                     />
                     <div
-                      className={` h-0.5 w-5 bg-white  rounded-md mt-1  ${isOpen ? '-rotate-45 -translate-x-0.5 ' : 'rotate-0 '}`}
+                      className={` h-0.5 w-5 bg-white ease-in duration-200  rounded-md mt-1  ${isOpen ? '-rotate-45 -translate-x-0.5 ' : 'rotate-0 '}`}
                     />
                   </div>
                 </div>
               </button>
               {isOpen && (
-                <div className="absolute w-full h-screen bg-black  border-white border-8 bg-dark shadow-lg z-10">
+                <AnimatePresence>
+                  <motion.div variants={fadeIn} initial="initial" whileInView="animate" exit="exit" viewport={{ once: false }} className="absolute ease-in duration-800  w-full h-screen bg-black  border-white border-8 bg-dark shadow-lg z-10">
                   <div className='flex flex-col h-full  content-between'>
                     <div className="flex flex-col mx-auto mt-20 text-center font-oswald tracking-light text-white w-10/12 z-20">
                       <div onClick={() => setIsOpen(!isOpen)} className="nav-link-container  py-2  border-b-2 border-white ">
@@ -76,8 +93,8 @@ function Navbar() {
                     
                   </div>
                   
-                </div>
-                
+                </motion.div>
+                </AnimatePresence>
               )}
             </div>
           </div>
