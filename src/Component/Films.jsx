01467f6457b0404c91876178eea9f0324f8674cb
ASHUTOSH KUMAR <ashutoshkumar.ash205@gmail.com>
2023-03-15 13:33:39 +0530

    text-changed

--- src/Component/Films.jsx
+++ src/Component/Films.jsx
@@ -15,15 +15,15 @@ function Films() {
         dropShadow={'drop-shadow-blue-1'}
           movieName={'Kashmiri Nazariya'}
           aboutmovie={'52 Min | Documentary | 2023'}
-          movieDescription={'Follows a journalist and a politician. The former to elicit the ground reality and sentiment for ‘azadi’ in South Kashmir and the latter to highlight the campaign for Indian democracy in a conflict torn state. The film focuses on the 2019 parliamentary election campaign and the historic events that followed.'}
+          movieDescription={'Follows the lives of a journalist and a politician. The former to elicit the ground reality and sentiment for ‘azadi’ (freedom) in South Kashmir and the latter to highlight the campaign for Indian democracy in a conflict torn state. The film focuses on the 2019 Parliamentary campaign and the historic events that followed.'}
         />
         <FilmsComponent
           img1={'/assets/Images/Movie/ChaiGarden-2.png'}
           img2={'/assets/Images/Movie/ChaiGarden-1.png'}
           dropShadow={'drop-shadow-orange-1'}
           movieName={'Chai Garden'}
-          aboutmovie={'27 Min | Documentary | 2023'}
-          movieDescription={'Follows the lives of exploited tea garden workers in West Bengal, India. The film focuses on the struggles of three neighbours. Jeevani, an alcohol vendor, Jyoti, a prohibition proponent and Leos, a tribal leader and minimum wage activist.'}
+          aboutmovie={'27 Min | Documentary | 2019'}
+          movieDescription={'Follows the lives of three neighbours living on tea plantations in West Bengal, India. A tribal leader protesting exploitative wages, an alcohol vendor trying to make ends meet, and a prohibition proponent empowering women. The film focuses on the struggle to live a fulfilling life in a region plagued by indigence and illiteracy.'}
         />
 
       </div>
